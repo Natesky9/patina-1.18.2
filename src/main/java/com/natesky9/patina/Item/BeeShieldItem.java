da8858a205a9537d3f8545a1ac19f5537549648b
Nate <Natesky9@gmail.com>
2023-06-14 02:47:39 -0400

    began 1.19.4 version

--- src/main/java/com/natesky9/patina/Item/BeeShieldItem.java
+++
@@ -1,9 +0,0 @@
-package com.natesky9.patina.Item;
-
-import net.minecraft.world.item.ShieldItem;
-
-public class BeeShieldItem extends ShieldItem {
-    public BeeShieldItem(Properties pProperties) {
-        super(pProperties);
-    }
-}
