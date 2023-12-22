f8d6f410f30fc3ebb0fb389330a8f9bf8ad1ddbe
Nate <Natesky9@gmail.com>
2023-12-22 17:15:53 -0500

    updated to forge 48.1, global loot modifiers now work

--- src/main/java/com/natesky9/patina/Loot/AddItemModifier.java
+++ src/main/java/com/natesky9/patina/Loot/AddItemModifier.java
@@ -2,6 +2,7 @@ package com.natesky9.patina.Loot;
 
 import com.google.common.base.Suppliers;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.PrimitiveCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.item.Item;
@@ -18,22 +19,26 @@ import java.util.function.Supplier;
 
 public class AddItemModifier extends LootModifier {
     public static final Supplier<Codec<AddItemModifier>> CODEC = Suppliers.memoize(
-            () -> RecordCodecBuilder.create(instance -> codecStart(instance).and(ForgeRegistries.ITEMS.getCodec()
-                    .fieldOf("item").forGetter(m -> m.item)).apply(instance, AddItemModifier::new)));
+            () -> RecordCodecBuilder.create(instance -> codecStart(instance)
+                    .and(ForgeRegistries.ITEMS.getCodec().fieldOf("item").forGetter(m -> m.item))
+                    .and(PrimitiveCodec.FLOAT.fieldOf("chance").forGetter(a -> a.chance))
+                    .apply(instance, AddItemModifier::new)));
     private final Item item;
+    private final float chance;
     /**
      * Constructs a LootModifier.
      *
      * @param conditionsIn the ILootConditions that need to be matched before the loot is modified.
      */
-    public AddItemModifier(LootItemCondition[] conditionsIn, Item item) {
+    public AddItemModifier(LootItemCondition[] conditionsIn, Item item, float chance) {
         super(conditionsIn);
         this.item = item;
+        this.chance = chance;
     }
 
     @Override
     public @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        //if (context.getRandom().nextFloat() >= .95F)
+        if (context.getRandom().nextFloat() < chance)
             generatedLoot.add(new ItemStack(item));
         return generatedLoot;
     }
--- src/main/java/com/natesky9/patina/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/com/natesky9/patina/datagen/ModGlobalLootModifiersProvider.java
@@ -1,17 +1,22 @@
 package com.natesky9.patina.datagen;
 
+import com.natesky9.patina.Loot.AddItemModifier;
 import com.natesky9.patina.Loot.AddMinMax;
 import com.natesky9.patina.Loot.AddSingleItemModifier;
+import com.natesky9.patina.Loot.ReplaceItemModifier;
 import com.natesky9.patina.Patina;
+import com.natesky9.patina.init.ModEntityTypes;
 import com.natesky9.patina.init.ModItems;
-import net.minecraft.advancements.critereon.EntityFlagsPredicate;
-import net.minecraft.advancements.critereon.EntityPredicate;
+import net.minecraft.advancements.critereon.*;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemEntityPropertyCondition;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
@@ -24,19 +29,43 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
 
     @Override
     protected void start() {
-        add("add_wither_fragment_1", new AddSingleItemModifier(new LootItemCondition[]{
+        add("add_wither_fragment_1", new AddItemModifier(new LootItemCondition[]{
                 new LootTableIdCondition(BuiltInLootTables.NETHER_BRIDGE)
-        }, ModItems.WITHER_FRAGMENT_1.get()));
-        //add("add_wither_fragment_2", new AddItemModifier(new LootItemCondition[]{
-        //        new LootTableIdCondition.Builder(new ResourceLocation("chests/ruined_portal")).build()
-        //}, ModItems.WITHER_FRAGMENT_1.get()));
-        //add("add_wither_fragment_3",new AddSingleItemModifier(new LootItemCondition[]{
-        //        new LootTableIdCondition.Builder(new ResourceLocation("chests/nether_bridge")).build()
-        //},ModItems.WITHER_FRAGMENT_3.get()));
-        //add("add_wither_fragment_4",new AddSingleItemModifier(new LootItemCondition[]{
-        //        new LootTableIdCondition.Builder(new ResourceLocation("entities/wither")).build()
-        //},ModItems.WITHER_FRAGMENT_4.get()));
+        }, ModItems.WITHER_FRAGMENT_1.get(),.1f));
+        add("add_wither_fragment_2", new AddItemModifier(new LootItemCondition[]{
+                new LootTableIdCondition.Builder(BuiltInLootTables.RUINED_PORTAL).build()
+        }, ModItems.WITHER_FRAGMENT_2.get(),.5f));
+        add("add_wither_fragment_3",new AddSingleItemModifier(new LootItemCondition[]{
+                LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS,
+                        EntityPredicate.Builder.entity().of(EntityType.WITHER)).build()
+        },ModItems.WITHER_FRAGMENT_3.get()));
+        add("add_wither_fragment_4",new AddSingleItemModifier(new LootItemCondition[]{
+                LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS,
+                                EntityPredicate.Builder.entity().of(EntityType.WITHER))
+                        .and(LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.KILLER,
+                                EntityPredicate.Builder.entity().of(EntityType.PIGLIN))).build()
+        },ModItems.WITHER_FRAGMENT_4.get()));
         //
+        add("add_bee_fragment_1", new AddItemModifier(new LootItemCondition[]{
+                LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.BEE_NEST).build()
+        },ModItems.BEE_FRAGMENT_1.get(),.1f));
+        add("add_bee_fragment_2", new AddItemModifier(new LootItemCondition[]{
+                LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS,
+                                EntityPredicate.Builder.entity().of(EntityType.BEE))
+                        .and(LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.KILLER,
+                                EntityPredicate.Builder.entity().equipment(EntityEquipmentPredicate.Builder.equipment()
+                                        .mainhand(ItemPredicate.Builder.item().hasEnchantment(
+                                                new EnchantmentPredicate(Enchantments.BANE_OF_ARTHROPODS, MinMaxBounds.Ints.atLeast(1)))))))
+                        .build()
+        },ModItems.BEE_FRAGMENT_2.get(), .1f));
+        add("add_bee_fragment_3", new AddSingleItemModifier(new LootItemCondition[]{
+                LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS,
+                        EntityPredicate.Builder.entity().of(ModEntityTypes.BEE_BOSS.get())).build()
+        },ModItems.BEE_FRAGMENT_3.get()));
+        add("add_bee_fragment_4",new AddSingleItemModifier(new LootItemCondition[]{
+                LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.KILLER,
+                        EntityPredicate.Builder.entity().of(EntityType.BEE)).build()
+        },ModItems.BEE_FRAGMENT_4.get()));
         add("turtle_shell_from_turtle",new AddSingleItemModifier(new LootItemCondition[]{
                 LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS,
                         EntityPredicate.Builder.entity().of(EntityType.TURTLE)
@@ -46,7 +75,7 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
                 new LootTableIdCondition.Builder(new ResourceLocation("entities/ender_dragon")).build()
         },4,8,ModItems.DRAGON_SCALE.get()));
         //
-        add("crab_claw_from_fishing",new AddSingleItemModifier(new LootItemCondition[]{
+        add("crab_claw_from_fishing",new ReplaceItemModifier(new LootItemCondition[]{
                 new LootTableIdCondition.Builder(new ResourceLocation("gameplay/fishing/treasure")).build()
         },ModItems.CLAW.get()));
     }
